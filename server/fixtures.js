b313d473ec398a145b26c181c79364d33ec5275e
nicolasservel <nico.servel@gmail.com>
2014-11-20 19:15:23 +0100

    hello

---
+++ server/fixtures.js
@@ -0,0 +1,17 @@
+if(Posts.find().count()=== 0){
+	Posts.insert({
+		title:'Introducing Telescope',
+		url:'http://sachargeif.com/Introducing-telescope/'
+});
+
+	Posts.insert({
+		title:'Meteor',
+		url:'http://meteor.com'
+});
+
+	Posts.insert({
+		title:'The Meteor Book',
+		url:'http://themeteorbook.com'
+});
+
+}
